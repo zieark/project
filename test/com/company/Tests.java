60fa21144c113df076a2ae544ff697990fb62696
zieark <w64246@student.wsiz.edu.pl>
2022-02-08 15:56:10 +0100

    add tests

---
+++ test/com/company/Tests.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests {
+
+    @Test
+    public void skiJumpingStarsTest() {
+        int stars = 24;
+        int maxStars = 24;
+        int starsFactor = stars/maxStars;
+        Assert.assertEquals(starsFactor,1);
+    }
+    @Test
+    public void footballStarsTest() {
+        int stars = 24;
+        int maxStars = 24;
+        int starsFactor = stars/maxStars;
+        Assert.assertEquals(starsFactor,1);
+    }
+    @Test
+    public void basketballStarsTest() {
+        int stars = 24;
+        int maxStars = 24;
+        int starsFactor = stars/maxStars;
+        Assert.assertEquals(starsFactor,1);
+    }
+    @Test
+    public void volleyballStarsTest() {
+        int stars = 24;
+        int maxStars = 24;
+        int starsFactor = stars/maxStars;
+        Assert.assertEquals(starsFactor,1);
+    }
+    @Test
+    public void tennisStarsTest() {
+        int stars = 24;
+        int maxStars = 24;
+        int starsFactor = stars/maxStars;
+        Assert.assertEquals(starsFactor,1);
+    }
+
+}
